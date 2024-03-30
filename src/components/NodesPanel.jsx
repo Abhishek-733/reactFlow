7a869b9ff2c073c0bc6b5e4ce949182df3dfe24e
Abhishek-733 <abhishekkrsingh095@gmail.com>
2024-03-30 22:09:43 +0530

    info Added

--- src/components/NodesPanel.jsx
+++ src/components/NodesPanel.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NodePreview from "./NodePreview";
 import { NodeTypes } from "./Nodes/nodeTypes";
+import { BsInfoCircleFill } from "react-icons/bs";
 
 function NodesPanel() {
   const onDragStart = (event, nodeType) => {
@@ -9,8 +10,15 @@ function NodesPanel() {
   };
   return (
     <div className="p-1 w-full">
-      <div className="text-gray-500 mb-1 text-xs">
-        You can drag these nodes to the pane on the right
+      <div className="text-gray-500 text-xs border-b border-gray-300 list-disc">
+        <div className="flex gap-2 items-center font-bold">
+          <BsInfoCircleFill />
+          Info
+        </div>
+        <li>You can drag these nodes to the plane on the right</li>
+        <li>Double click on Node to delete it</li>
+        <li>Double click on edge to delete it</li>
+        <li>Click on Node to edit message.</li>
       </div>
 
       <div className="flex flex-col gap-2">
